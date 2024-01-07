56f5366b69efc46e15a6d1dfb72b1cbd92dea28f
kwangyoulsagong <sgky0511@naver.com>
2024-01-07 16:50:25 +0900

    feat: api 연결 코드 및 디자인, 코드 가독성

--- src/pages/view.js
+++ src/pages/view.js
@@ -1,5 +1,29 @@
 import React from "react";
+import axios from "axios";
+import "./view.css";
+import PostPreview from "../components/postpreview";
+const url = window.location.href;
+//get 방식
+axios
+  .get("/api/post/preview", {
+    params: {
+      url: url,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 const View = () => {
-  return <div>사용자들 메모 보기</div>;
+  return (
+    <div className="viewBackground">
+      <PostPreview />
+    </div>
+  );
 };
 export default View;

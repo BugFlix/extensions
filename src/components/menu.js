f23c14b2956e82303a82ee3a6bc9256aacf618ad
kwangyoulsagong <sgky0511@naver.com>
2024-01-09 14:49:31 +0900

    feat: 마이포스트 프리뷰 구현

---
+++ src/components/menu.js
@@ -0,0 +1,133 @@
+import React, { useState, useRef } from "react";
+import useDragDetect from "../hooks/useDragDetect";
+import useSelectionChange from "../hooks/useSelectionChange";
+import useStyledContent from "../hooks/useStyledContent";
+import Scrab from "../pages/scrab";
+import View from "../pages/view";
+import Post from "./post";
+import Tag from "./tag";
+import Title from "./title";
+import axios from "axios";
+import Update from "../pages/update";
+
+const Menu = () => {
+  const [selectedHtml, setSelectedHtml] = useState("");
+  const [computedStyles, setComputedStyles] = useState({});
+  const [isDragging, setIsDragging] = useState(false);
+  const letters = ["E", "V", "U"]; // Add more letters if needed
+  const [selectedLetter, setSelectedLetter] = useState("E");
+  const [selectedComponent, setSelectedComponent] = useState("E");
+  const toggleSelected = (letter) => {
+    setSelectedLetter((prevSelectedLetter) =>
+      prevSelectedLetter === letter ? null : letter
+    );
+    setSelectedComponent((prevSelectedComponent) =>
+      prevSelectedComponent === letter ? null : letter
+    );
+  };
+
+  const getCircleStyle = (letter) => ({
+    backgroundColor: selectedLetter === letter ? "#AAB3FF" : "",
+    color: selectedLetter === letter ? "#ffffff" : "",
+    height: "100%",
+    width: "21px",
+    borderRadius: "50%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  });
+  const handleSelectionChange = useSelectionChange(
+    setSelectedHtml,
+    setComputedStyles,
+    isDragging
+  );
+
+  useDragDetect(handleSelectionChange, setIsDragging);
+
+  const getStyledContent = useStyledContent(computedStyles, selectedHtml);
+  const titleRef = useRef();
+  const tagRef = useRef();
+  const scrabRef = useRef();
+  const postRef = useRef();
+
+  const getTitleHtml = () => {
+    if (titleRef && titleRef.current) {
+      return titleRef.current.getInnerHTML();
+    }
+  };
+  const getTagHtml = () => {
+    if (tagRef && tagRef.current) {
+      return tagRef.current.getInnerHTML();
+    }
+  };
+  const getScrabHtml = () => {
+    if (scrabRef && scrabRef.current) {
+      return scrabRef.current.getInnerHTML();
+    }
+    return "";
+  };
+  const getPostHtml = () => {
+    if (postRef && postRef.current) {
+      return postRef.current.getInnerHTML();
+    }
+    return "";
+  };
+  const saveMemo = () => {
+    const titleHtml = getTitleHtml();
+    const tagHtml = getTagHtml();
+    const scrabHtml = getScrabHtml();
+    const postHtml = getPostHtml();
+    const requestData = {
+      data: [
+        {
+          title: titleHtml,
+          tag: tagHtml,
+          content: scrabHtml,
+          memo: postHtml,
+          url: window.location.href,
+          image_url:
+            "https://blog.kakaocdn.net/dn/clyrhv/btqXJVvfOgF/1lMKjoQo3iW0pyYDmV2HVK/img.jpg",
+        },
+      ],
+    };
+    console.log(requestData);
+    const response = axios.post("/api/post", requestData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response.data);
+  };
+
+  return (
+    <div className="Container">
+      <div className="Menu-Container">
+        {letters.map((letter) => (
+          <span
+            key={letter}
+            className={letter}
+            onClick={() => toggleSelected(letter)}
+            style={getCircleStyle(letter)}
+          >
+            {letter}
+          </span>
+        ))}
+      </div>
+      {selectedComponent === "E" && (
+        <>
+          <Title ref={titleRef} />
+          <Tag ref={tagRef} />
+          <Scrab ref={scrabRef} getStyledContent={getStyledContent} />
+          <Post ref={postRef} />
+          <button className="saveBtn" onClick={saveMemo}>
+            저장
+          </button>
+        </>
+      )}
+      {selectedComponent === "V" && <View />}
+      {selectedComponent === "U" && <Update />}
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/pages/Unloginextentions.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "../App.css";
+import closeBtnImg from "../../src/images/closeBtn.png";
+import openBtnImg from "../../src/images/logo.png";
+import Header from "../components/header";
+import Account from "./Account";
+import UnloginMenu from "../components/unloginmenu";
+const Extensions = ({ onLoginSuccess }) => {
+  const [isContainerVisible, setIsContainerVisible] = useState(false);
+
+  const [btnClass, setBtnClass] = useState("openBtn");
+
+  //보이기 숨기기 버튼
+  const toggleContainerVisibility = () => {
+    setIsContainerVisible((prev) => !prev);
+    setBtnClass((prevClass) =>
+      prevClass === "openBtn" ? "closeBtn" : "openBtn"
+    );
+  };
+
+  return (
+    <div>
+      <div className={btnClass} onClick={toggleContainerVisibility}>
+        <img
+          src={btnClass === "openBtn" ? openBtnImg : closeBtnImg}
+          alt="Button"
+        />
+      </div>
+      {isContainerVisible && (
+        <div className="extension-content">
+          <UnloginMenu />
+          <Account onLoginSuccess={onLoginSuccess} />
+          <Header />
+        </div>
+      )}
+    </div>
+  );
+};
+export default Extensions;
